982eb967c28941dadb4fd84d19b9d63a6583ebe0
Joesta Sebolela <joestax@icloud.com>
2025-12-12 16:42:16 +0200

    modified sessionFilterChain to use builder pattern and configured session creation to STATELESS

--- src/main/java/com/telusko/SpringEcom/config/SecurityConfig.java
+++ src/main/java/com/telusko/SpringEcom/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -18,10 +19,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, ClientHttpMessageConvertersCustomizer clientConvertersCustomizer) throws Exception {
 
-        http.csrf(csrCustomizer -> csrCustomizer.disable());
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
+        return http.csrf(csrCustomizer -> csrCustomizer.disable())
+        .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+        .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
 
-        return http.build();
     }
 }
